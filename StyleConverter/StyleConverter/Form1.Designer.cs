ec63490715ba7feaa74f895bbff7f42cdaf86043
Jermayne Williams <jermayne21@gmail.com>
2019-05-05 15:34:10 -0600

    Add files via upload

---
+++ StyleConverter/StyleConverter/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace StyleConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RichTextbox = new System.Windows.Forms.RichTextBox();
+            this.ConvertedTextbox = new System.Windows.Forms.RichTextBox();
+            this.ConvertBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // RichTextbox
+            // 
+            this.RichTextbox.Location = new System.Drawing.Point(12, 54);
+            this.RichTextbox.Name = "RichTextbox";
+            this.RichTextbox.Size = new System.Drawing.Size(601, 743);
+            this.RichTextbox.TabIndex = 0;
+            this.RichTextbox.Text = "";
+            // 
+            // ConvertedTextbox
+            // 
+            this.ConvertedTextbox.Location = new System.Drawing.Point(822, 54);
+            this.ConvertedTextbox.Name = "ConvertedTextbox";
+            this.ConvertedTextbox.ReadOnly = true;
+            this.ConvertedTextbox.Size = new System.Drawing.Size(601, 743);
+            this.ConvertedTextbox.TabIndex = 1;
+            this.ConvertedTextbox.Text = "";
+            // 
+            // ConvertBtn
+            // 
+            this.ConvertBtn.Location = new System.Drawing.Point(648, 374);
+            this.ConvertBtn.Name = "ConvertBtn";
+            this.ConvertBtn.Size = new System.Drawing.Size(130, 36);
+            this.ConvertBtn.TabIndex = 2;
+            this.ConvertBtn.Text = "Convert";
+            this.ConvertBtn.UseVisualStyleBackColor = true;
+            this.ConvertBtn.Click += new System.EventHandler(this.ConvertBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(226, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Rich Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1054, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Converted Text";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1476, 809);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ConvertBtn);
+            this.Controls.Add(this.ConvertedTextbox);
+            this.Controls.Add(this.RichTextbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox RichTextbox;
+        private System.Windows.Forms.RichTextBox ConvertedTextbox;
+        private System.Windows.Forms.Button ConvertBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ StyleConverter/StyleConverter/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StyleConverter
+{
+    public partial class Form1 : Form
+    {
+        private RichTextBox richText, convertedText;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            richText = (RichTextBox) this.Controls.Find("RichTextbox", false)[0];
+            convertedText = (RichTextBox) this.Controls.Find("ConvertedTextbox", false)[0];
+        }
+
+        private void ConvertBtn_Click(object sender, EventArgs e)
+        {
+            convertedText.Clear();
+            bool isH2 = false;
+            bool isBold = false;
+            bool isLink = false;
+            for (int index = 0; index < richText.Text.Length; index++)
+            {
+                richText.Select(index, 1);
+                //H2 text
+                if (richText.SelectionFont.Size == 14  && richText.SelectionFont.Bold)
+                {
+                    //H2 STUFF
+                    if (!isH2)
+                    {
+                        convertedText.Text += "<h2>";
+                        isH2 = true;
+                    }
+                    if (richText.SelectedText != "\n") convertedText.Text += richText.SelectedText;
+                } 
+                else
+                {
+
+                    if (isH2)
+                    {
+                        isH2 = false;
+                        convertedText.Text += "</h2>" + "\n\n";
+
+                    }
+
+                }
+                
+                //bold text
+                if(richText.SelectionFont.Size < 14 && richText.SelectionFont.Bold)
+                {
+                    if (!isBold)
+                    {
+                        convertedText.Text += "<b>";
+                        isBold = true;
+                    }
+                    if (richText.SelectedText == "\n")
+                    {
+                        isBold = false;
+                        convertedText.Text += "</b>";
+                    }
+                    else convertedText.Text += richText.SelectedText;
+                    
+                }
+                else
+                {
+                    if (isBold)
+                    {
+                        isBold = false;
+                        convertedText.Text += "</b>";
+
+                    }
+                }
+
+                // a href link
+                if (richText.SelectionFont.Underline)
+                {
+                    if (!isLink)
+                    {
+                        convertedText.Text += "<a href=\"\" >";
+                        isLink = true;
+                    }
+                    convertedText.Text += richText.SelectedText;
+                }
+                else
+                {
+                    if (isLink)
+                    {
+                        isLink = false;
+                        convertedText.Text += "</a>";
+
+                    }
+                }
+
+
+                if (!isH2 && !isBold && !isLink)
+                {
+                    convertedText.Text += richText.SelectedText;
+                    if (richText.SelectedText == "\n") convertedText.Text += "\n";
+                }
+            }
+        }
+    }
+}
